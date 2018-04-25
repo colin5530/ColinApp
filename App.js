83cbeb9a3f7485742aa05c35798a5fa1a648c88f
colin5530 <colinmcdermott90@gmail.com>
2018-04-25 21:40:04 +0100

    Full nav stack

--- App.js
+++ App.js
@@ -5,25 +5,52 @@ import { StackNavigator } from 'react-navigation';
 import store from './src/stores/store'; //Import the store
 import HomeScreen from './src/home'; //Import the component file
 import Details from './src/details';
+import ModalScreen from './src/components/modal';
 
-const RootStack = StackNavigator({
-    Home: {
-        screen: HomeScreen,
-    },
-    Details: {
-        screen: Details,
-    },
+console.disableYellowBox = true;
+
+const MainStack = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Details: {
+    screen: Details,
+  },
 },
 {
-    initialRouteName: 'Home',
+  initialRouteName: 'Home',
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }
 });
 
+const RootStack = StackNavigator(
+  {
+    Main: {
+      screen: MainStack,
+    },
+    MyModal: {
+      screen: ModalScreen,
+    },
+  },
+  {
+    mode: 'modal',
+    headerMode: 'none',
+  }
+);
+
 export default class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <RootStack />
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <RootStack />
+      </Provider>
+    );
+  }
 }
--- src/home.js
+++ src/home.js
@@ -3,15 +3,40 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 // import { StackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import Counter from './components/counter';
+import LogoTitle from './components/logoTitle';
+import ModalScreen from './components/modal';
 
 const mapStateToProps = state => {
   return { count: state };
 };
 
 class ConnectedHome extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    // const params = navigation.state.params || {};
+    
+    return {
+      title: 'Home',
+      // headerTitle: <LogoTitle/>,
+      headerLeft: (
+        <Button
+          backgroundColor="#f00"
+          onPress={() => navigation.navigate('MyModal')}
+          title="Info"
+        />
+      ),
+    };
+  };
+
   componentDidUpdate() {
     if (this.props.count >= 10) {
-      this.props.navigation.navigate('Details', {clickedThrough: true, clicks: this.props.count});
+      this.props.navigation.navigate(
+        'Details', 
+        {
+          clickedThrough: true, 
+          clicks: this.props.count,
+          headerTitle: 'Clicks Navigation Title'
+        }
+      );
     }
   }
   
@@ -22,7 +47,13 @@ class ConnectedHome extends React.Component {
         <View>
           <Button
             title="Go to Details"
-            onPress={() => this.props.navigation.navigate('Details')}
+            onPress={() => this.props.navigation.navigate(
+              'Details', 
+              {
+                clicks: this.props.count,
+                headerTitle: 'Navigation Title',
+              }
+            )}
           />
         </View>
       </View>
